4263acf2eb070c7b0bc2aaac6b162046baa18539
Dennis Addo <dennis_addo@aol.com>
2017-10-29 18:41:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function theBeatlesPlay(musicians, instruments) {
-  var sent;
+  var sent = new Array();
   for (let i = 0; i < musicians.length; i++) {
     sent[i] = `"${musicians[i]} plays ${instruments[i]}"`;
   }
